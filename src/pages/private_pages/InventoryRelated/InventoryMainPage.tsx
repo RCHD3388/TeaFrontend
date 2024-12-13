cae7424b986ac077279d3b567d966937bac1bbe0
RCHD3388 <richardraferguy@gmail.com>
2024-12-13 21:27:32 +0700

    project tab split

---
+++ src/pages/private_pages/InventoryRelated/InventoryMainPage.tsx
@@ -0,0 +1,78 @@
+import { Autocomplete, Box, Button, TextField } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import AddSupplier from "../../../components/supplier_related/AddSupplier";
+import { GetAllSuppliersDocument } from "../../../graphql/supplier.generated";
+import { useQuery } from "@apollo/client";
+import StickyHeadTable, { StickyHeadTableColumn } from "../../../components/global_features/StickyHeadTable";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import { GetAllWarehousesDocument } from "../../../graphql/inventory.generated";
+import CustomListComponent from "../../../components/inventory_related/ComponentCard";
+import { WarehouseTypeValues } from "../../../types/staticData.types";
+
+export default function InventoryMainPage() {
+  let { data, loading, error, refetch } = useQuery(GetAllWarehousesDocument, { variables: { requiresAuth: true } })
+  const navigate = useNavigate()
+
+  const [nameFilter, setNameFilter] = useState("")
+  const [statusFilter, setStatusFilter] = useState("")
+  const [warehouseFilter, setWarehouseFilter] = useState("")
+
+  useEffect(() => { if (data) refetch() }, [data, refetch])
+
+  return (
+    <div className="flex flex-col" style={{ maxHeight: "100%" }}>
+
+      <Box display={"flex"} flexWrap={"wrap"}>
+        <TextField
+          color="secondary" sx={{ mb: 1, mr: 1 }} label="Pencarian" size='small' variant="outlined"
+          onChange={(e) => { setNameFilter(e.target.value) }}
+          slotProps={{
+            input: {
+              startAdornment: <SearchIcon></SearchIcon>,
+            },
+          }}
+        />
+        <Autocomplete
+          disablePortal
+          options={["Active", "Inactive"]}
+          sx={{ width: 300, mb: 1, mr: 1 }}
+          onChange={(event: React.SyntheticEvent, newValue: string | null) => {
+            setStatusFilter(newValue || "")
+          }}
+          renderInput={(params) => <TextField color="secondary" {...params} size="small" label="Status Pegawai" />}
+        />
+        <Autocomplete
+          disablePortal
+          options={WarehouseTypeValues}
+          sx={{ width: 300, mb: 1, mr: 1 }}
+          onChange={(event: React.SyntheticEvent, newValue: string | null) => {
+            setWarehouseFilter(newValue || "")
+          }}
+          renderInput={(params) => <TextField color="secondary" {...params} size="small" label="Tipe Gudang" />}
+        />
+      </Box>
+
+      {!loading && !error && <>
+        {data.getAllWarehouses.length == 0 ?
+          <div className="flex justify-center items-center p-5 bg-primary shadow-md">
+            PERUSAHAAN BELUM MEMILIKI WAREHOUSE
+          </div>
+
+          : data.getAllWarehouses.map((item: any, index: number) => {
+            console.log(item)
+            if (nameFilter != "" && !item.name.toLowerCase().includes(nameFilter.toLowerCase())) return null
+            if (statusFilter != "" && item.status != statusFilter) return null
+            if (warehouseFilter != "" && item.type != warehouseFilter) return null
+
+            return <CustomListComponent key={index} title={item.name}
+              type={item.type} status={item.status}
+              description={item.description}
+              onDetailClick={() => { navigate(`/appuser/inventory/${item._id}`) }}
+            />
+          })}
+      </>}
+    </div>
+  );
+}
--- src/pages/private_pages/InventoryRelated/InventoryPage.tsx
+++ src/pages/private_pages/InventoryRelated/InventoryPage.tsx
@@ -1,25 +1,16 @@
-import { Autocomplete, Box, Button, TextField } from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
-import AddSupplier from "../../../components/supplier_related/AddSupplier";
-import { GetAllSuppliersDocument } from "../../../graphql/supplier.generated";
-import { useQuery } from "@apollo/client";
-import StickyHeadTable, { StickyHeadTableColumn } from "../../../components/global_features/StickyHeadTable";
+import { Autocomplete, Box, Button, Tab, Tabs, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { a11yProps, CustomTabPanel } from "../../../components/CustomTabPanel";
+import InventoryMainPage from "./InventoryMainPage";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import SearchIcon from '@mui/icons-material/Search';
-import { GetAllWarehousesDocument } from "../../../graphql/inventory.generated";
-import CustomListComponent from "../../../components/inventory_related/ComponentCard";
-import { WarehouseTypeValues } from "../../../types/staticData.types";
 
 export default function InventoryPage() {
-  let { data, loading, error, refetch } = useQuery(GetAllWarehousesDocument, { variables: { requiresAuth: true } })
   const navigate = useNavigate()
+  const [value, setValue] = React.useState(0);
 
-  const [nameFilter, setNameFilter] = useState("")
-  const [statusFilter, setStatusFilter] = useState("")
-  const [warehouseFilter, setWarehouseFilter] = useState("")
-
-  useEffect(() => { if (data) refetch() }, [data, refetch])
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <div className="p-5" style={{ height: "100%" }}>
@@ -27,48 +18,33 @@ export default function InventoryPage() {
         {/* page header title */}
         <div className="text-4xl font-bold mb-2">Inventori Perusahaan</div>
 
-        <Box display={"flex"} flexWrap={"wrap"}>
-          <TextField
-            color="secondary" sx={{ mb: 1, mr: 1 }} label="Pencarian" size='small' variant="outlined"
-            onChange={(e) => { setNameFilter(e.target.value) }}
-            slotProps={{
-              input: {
-                startAdornment: <SearchIcon></SearchIcon>,
-              },
-            }}
-          />
-          <Autocomplete
-            disablePortal
-            options={["Active", "Inactive"]}
-            sx={{ width: 300, mb: 1, mr: 1 }}
-            onChange={(event: React.SyntheticEvent, newValue: string | null) => {
-              setStatusFilter(newValue || "")
-            }}
-            renderInput={(params) => <TextField color="secondary" {...params} size="small" label="Status Pegawai" />}
-          />
-          <Autocomplete
-            disablePortal
-            options={WarehouseTypeValues}
-            sx={{ width: 300, mb: 1, mr: 1 }}
-            onChange={(event: React.SyntheticEvent, newValue: string | null) => {
-              setWarehouseFilter(newValue || "")
-            }}
-            renderInput={(params) => <TextField color="secondary" {...params} size="small" label="Tipe Gudang" />}
-          />
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider', top: "0%", backgroundColor: "white" }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" indicatorColor='secondary'>
+              <Tab label="Data Warehouse" {...a11yProps(0)} sx={{
+                color: value === 0 ? 'secondary.main' : 'inherit',
+                '&.Mui-selected': { color: 'secondary.main' },
+              }} />
+              <Tab label="Data Material" {...a11yProps(1)} sx={{
+                color: value === 1 ? 'secondary.main' : 'inherit',
+                '&.Mui-selected': { color: 'secondary.main' },
+              }} />
+              <Tab label="Data Peralatan" {...a11yProps(1)} sx={{
+                color: value === 2 ? 'secondary.main' : 'inherit',
+                '&.Mui-selected': { color: 'secondary.main' },
+              }} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            <InventoryMainPage/>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            
+          </CustomTabPanel>
         </Box>
-
-        {!loading && !error && data.getAllWarehouses.map((item: any, index: number) => {
-          console.log(item)
-          if (nameFilter != "" && !item.name.toLowerCase().includes(nameFilter.toLowerCase())) return null
-          if (statusFilter != "" && item.status != statusFilter) return null
-          if (warehouseFilter != "" && item.type != warehouseFilter) return null
-
-          return <CustomListComponent key={index} title={item.name}
-            type={item.type} status={item.status}
-            description={item.description}
-            onDetailClick={() => { navigate(`/appuser/inventory/${item._id}`) }}
-          />
-        })}
       </div>
     </div>
   );
